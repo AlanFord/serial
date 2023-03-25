b88bfabb0fec4a05ff3235f9ffe401f2d1518a8f
Alan Ford <alanford354@gmail.com>
2023-03-25 09:24:27 -0400

    01_send working for nucleo F746ZG board

---
+++ 01_send/01_send_arduino/01_send_arduino.ino
@@ -0,0 +1,19 @@
+/*
+Basic sketch to test serial transmission from microcontroller to arduino ide
+
+Counts from 1 to 10, sending the value to the computer via serial
+*/
+void setup() {
+  //Initialize serial and wait for port to open:
+  Serial.begin(9600);
+  while (!Serial) {
+    ;  // wait for serial port to connect. Needed for native USB port only
+  }
+}
+
+void loop() {
+  for (int i = 0; i < 11; i++) {
+    Serial.println(i);
+    delay(1000);
+  }
+}
