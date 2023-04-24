53e8cf969fba549fc1d3816e4511344a7c9ecb00
Alan Ford <alanford354@gmail.com>
2023-04-24 12:35:21 -0400

    added a qt graph

--- 05_threads/05_threads_pyqt5/05_threads_pyqt5.py
+++ 05_threads/05_threads_pyqt5/05_threads_pyqt5.py
@@ -7,17 +7,25 @@ tab characters. Values are integers in ASCII encoding.
 """
 import sys
 import time
+from random import randint
 
-from PyQt5.QtCore import Qt, QCoreApplication, QThread, pyqtSignal, pyqtSlot
+from PyQt5.QtCore import (
+    Qt,
+    QCoreApplication,
+    QThread,
+    pyqtSignal,
+    pyqtSlot,
+    QTimer
+)
 from PyQt5.QtWidgets import (
     QApplication,
     QLabel,
     QMainWindow,
     QPushButton,
-    QSpinBox,
     QVBoxLayout,
-    QWidget,
+    QWidget
 )
+import pyqtgraph as pg  # import PyQtGraph after Qt
 
 
 class Thread(QThread):
@@ -62,7 +70,6 @@ class Thread(QThread):
         self.is_running = False
 
 
-
 class MainWindow(QMainWindow):
     def __init__(self):
         super().__init__()
@@ -83,6 +90,26 @@ class MainWindow(QMainWindow):
         self.status = QLabel("Starting...")
         self.status.setAlignment(Qt.AlignCenter)
 
+        # A plot
+        self.graphWidget = pg.PlotWidget()
+
+        self.x = list(range(100))  # 100 time points
+        self.y = [
+            randint(0, 100) for _ in range(100)
+        ]  # 100 data points
+
+        self.graphWidget.setBackground("w")
+
+        pen = pg.mkPen(color=(255, 0, 0))
+        self.data_line = self.graphWidget.plot(
+            self.x, self.y, pen=pen
+        )  # <1>
+
+        self.timer = QTimer()
+        self.timer.setInterval(50)
+        self.timer.timeout.connect(self.update_plot_data)
+        self.timer.start()
+
         # Window layout
         container = QWidget()
         layout = QVBoxLayout()
@@ -90,6 +117,7 @@ class MainWindow(QMainWindow):
         layout.addWidget(button2)
         layout.addWidget(button3)
         layout.addWidget(self.status)
+        layout.addWidget(self.graphWidget)
         container.setLayout(layout)
 
         # Start the Show!
@@ -117,6 +145,19 @@ class MainWindow(QMainWindow):
         self.thread.stop()
         # Terminate the exec_ loop
         QCoreApplication.quit()
+
+    def update_plot_data(self):
+
+        self.x = self.x[1:]  # Remove the first y element.
+        self.x.append(
+            self.x[-1] + 1
+        )  # Add a new value 1 higher than the last.
+
+        self.y = self.y[1:]  # Remove the first
+        self.y.append(randint(0, 100))  # Add a new random value.
+
+        self.data_line.setData(self.x, self.y)  # Update the data.
+
 
 app = QApplication(sys.argv)
 window = MainWindow()
