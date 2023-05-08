623722e42a549b96ff65149edbe8e09df7ae9aa8
Alan Ford <alanford354@gmail.com>
2023-05-08 13:01:46 -0400

    switch to a QPixmap for plotting

--- 05_threads/05_threads_pyqt5/05_threads_pyqt5.py
+++ 05_threads/05_threads_pyqt5/05_threads_pyqt5.py
@@ -4,10 +4,19 @@ produces 2 values per line every 500ms. Each line looks like:
 WOG   1.00    -2.00
 with each data line starting with "WOG" and each field separated by
 tab characters. Values are integers in ASCII encoding.
+
+Two classes are used
+    - MainWindow handles all of the graphics, including generating
+    the data plot
+    - Threaded handles the serial communication in a separate
+    thread.
+    - The main() function does the following:
+        - establishes the serial connection
+        - creates instances of the aformentioned classes
+        - creates the signal/slot connections between the instances
 """
 import sys
 import time
-from random import randint
 from serial import Serial, SerialException
 
 from PyQt5.QtCore import (
@@ -16,7 +25,6 @@ from PyQt5.QtCore import (
     QThread,
     pyqtSignal,
     pyqtSlot,
-    QTimer
 )
 from PyQt5.QtWidgets import (
     QApplication,
@@ -26,9 +34,13 @@ from PyQt5.QtWidgets import (
     QVBoxLayout,
     QWidget
 )
-import pyqtgraph as pg  # import PyQtGraph after Qt
+
+from PyQt5.QtGui import QPixmap, QPainter, QPolygon
+
+# import pyqtgraph as pg  # import PyQtGraph after Qt
 
 
+# ==========================================================
 class Thread(QThread):
     """
     Worker thread.
@@ -37,7 +49,7 @@ class Thread(QThread):
     Reads from serial port ONLY if data waiting.
     """
 
-    result = pyqtSignal(str)
+    result = pyqtSignal(float, float)
 
     def __init__(self, serialPort):
         super().__init__()
@@ -51,17 +63,17 @@ class Thread(QThread):
         while self.is_running:
             if self.serialPort.inWaiting() != 0:
                 # Caution: the following line is BLOCKING
-                line = self.serialPort.readline()
+                msg = self.serialPort.readline()
                 # self.queue.put(line)
-                line = line.decode('ascii').strip("\r\n")
+                msg = msg.decode('ascii').strip("\r\n")
                 # Check contents of message,
-                if line[0:3] != "WOG":
-                    print("Bad Message: ", line)  # line not valid
+                if msg[0:3] != "WOG":
+                    print("Bad Message: ", msg)  # line not valid
                 else:
                     try:
-                        line = line[3:].strip()
-                        print("WOG line:",line)
-                        self.result.emit(line)
+                        data = msg.split("\t")
+                        x, y = float(data[1]), float(data[2])
+                        self.result.emit(x, y)
                     except Exception as e:
                         print(e)
 
@@ -77,7 +89,7 @@ class Thread(QThread):
         # note the string termination
         self.serialPort.write(bytes('L\n', 'UTF-8'))
 
-    # @pyqtSlot()
+    @pyqtSlot()
     def start_remote(self):
         """
         Event handler for the Start button
@@ -86,6 +98,7 @@ class Thread(QThread):
         self.serialPort.write(bytes('H\n', 'UTF-8'))
 
 
+# ==========================================================
 class MainWindow(QMainWindow):
     def __init__(self):
         super().__init__()
@@ -94,33 +107,28 @@ class MainWindow(QMainWindow):
         self.button1 = QPushButton("START")
         self.button2 = QPushButton("STOP")
         self.button3 = QPushButton("Done")
-        # button1.pressed.connect(self.start_button)
-        # button2.pressed.connect(self.stop_button)
-        # button3.pressed.connect(self.done_button)
 
-        # >>>  TEMPORARY: DELETE when plot is added
-        self.status = QLabel("Starting...")
-        self.status.setAlignment(Qt.AlignCenter)
+        # create a plot from a label widget
+        self.label = QLabel()
+        self.canvasWidth = 400
+        self.canvasHalfHeight = 150
+        canvas = QPixmap(self.canvasWidth, self.canvasHalfHeight*2)
+        canvas.fill(Qt.white)
+        self.label.setPixmap(canvas)
 
-        # A plot
-        self.graphWidget = pg.PlotWidget()
+        # these lists hold data for the two lines to be plotted
+        self.npoints = 100
+        self.Line1 = [0 for x in range(self.npoints)]
+        self.Line2 = [0 for x in range(self.npoints)]
 
-        self.x = list(range(100))  # 100 time points
-        self.y = [
-            randint(0, 100) for _ in range(100)
-        ]  # 100 data points
+        # painter = QPainter(self.label.pixmap())
+        # painter.drawPolyLine
+        # painter.end()
 
-        self.graphWidget.setBackground("w")
-
-        pen = pg.mkPen(color=(255, 0, 0))
-        self.data_line = self.graphWidget.plot(
-            self.x, self.y, pen=pen
-        )  # <1>
-
-        # self.timer = QTimer()
-        # self.timer.setInterval(50)
-        # self.timer.timeout.connect(self.update_plot_data)
-        # self.timer.start()
+        # pen = pg.mkPen(color=(255, 0, 0))
+        # self.data_line = self.graphWidget.plot(
+        #     self.x, self.y, pen=pen
+        # )
 
         # Window layout
         container = QWidget()
@@ -128,49 +136,61 @@ class MainWindow(QMainWindow):
         layout.addWidget(self.button1)
         layout.addWidget(self.button2)
         layout.addWidget(self.button3)
-        layout.addWidget(self.status)
-        layout.addWidget(self.graphWidget)
+        layout.addWidget(self.label)
         container.setLayout(layout)
 
         # Start the Show!
         self.setCentralWidget(container)
         self.show()
 
-    # def stop_button(self):
-    #     """
-    #     Event handler for the Stop button
-    #     """
-    #     # note the string termination
-    #     # self.serialPort.write(bytes('L\n', 'UTF-8'))
-    #     self.status.setText("STOP pressed")
-
-    # def start_button(self):
-    #     """
-    #     Event handler for the Start button
-    #     """
-    #     # note the string termination
-    #     # self.serialPort.write(bytes('H\n', 'UTF-8'))
-    #     self.status.setText("START pressed")
-
     @pyqtSlot()
     def done_button(self):
         # Terminate the exec_ loop
         QCoreApplication.quit()
 
-    @pyqtSlot(str)
-    def update_plot_data(self, badaboom):
-        print(badaboom)
-"""         self.x = self.x[1:]  # Remove the first x element.
-        self.x.append(
-            self.x[-1] + 1
-        )  # Add a new value 1 higher than the last.
+    @pyqtSlot(float, float)
+    def update_plot_data(self, x, y):
+        # scale and transform the data
+        print("x is ", x, ", y is ", y)
+        self.Line1 = self.Line1[1:]  # Remove the first element.
+        self.Line1.append(int(x))  # add the new value.
 
-        self.y = self.y[1:]  # Remove the first y element.
-        self.y.append(randint(0, 100))  # Add a new random value.
+        self.Line2 = self.Line2[1:]  # Remove the first element.
+        self.Line2.append(int(y))  # Add the new value.
+        """
+        Update the canvas graph lines from the cached data lists.
+        The lines are scaled to match the canvas size as the window may
+        be resized by the user.
+        """
+        w = self.canvasWidth
+        hh = self.canvasHalfHeight
+        max_X = max(self.Line1) + 1e-5
+        min_X = min(self.Line1) - 1e-5
+        max_Y = max(self.Line2) + 1e-5
+        min_Y = min(self.Line2) - 1e-5
+        max_all = max(max_X, -min_X, max_Y, -min_Y)
+        coordsX, coordsY = [], []
+        for n in range(0, self.npoints):
+            x = (w * n) / self.npoints
+            coordsX.append(x)
+            coordsX.append(hh * (1 + self.Line1[n] / max_all))
+            # coordsX.append(h - ((h * (self.Line1[n]+100)) / max_all))
+            coordsY.append(x)
+            coordsY.append(hh * (1 + self.Line2[n] / max_all))
+            # coordsY.append(h - ((h * (self.Line2[n]+100)) / max_all))
+        canvas = self.label.pixmap()
+        canvas.fill(Qt.white)
+        painter = QPainter(self.label.pixmap())
+        myLine = QPolygon()
+        myLine.setPoints(coordsX)
+        painter.drawPolygon(myLine)
+        painter.end()
 
-        self.data_line.setData(self.x, self.y)  # Update the data.
- """
+        # self.canvas.coords('X', *coordsX)
+        # self.canvas.coords('Y', *coordsY)
 
+
+# ==========================================================
 def main(args=None):
     if args is None:
         args = sys.argv
